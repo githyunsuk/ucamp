40d50f9c6d1f7774d8f5159a1a2d75e40bdc9dcf
hyunsuk_ju <145447772+githyunsuk@users.noreply.github.com>
2025-09-23 22:06:21 +0900

    Delete Hello_Spring_Web directory

--- Hello_Spring_Web/src/main/java/com/ucamp/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ucamp.repository;
-
-import com.ucamp.entity.BoardDTO;
-
-public interface BoardRepository {
-
-	public BoardDTO findBoardByNo(Long No);
-}
--- Hello_Spring_Web/src/main/java/com/ucamp/service/BoardServie.java
+++
@@ -1,9 +0,0 @@
-package com.ucamp.service;
-
-import com.ucamp.entity.BoardDTO;
-
-public interface BoardServie {
-
-	public BoardDTO findBoardByNo(Long no);
-	
-}
